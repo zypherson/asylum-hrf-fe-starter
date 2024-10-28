955d706f833ce32bf1baba3137f01696a0ec4ed1
Tom Tarpey <tomtarpeydev@gmail.com>
2024-10-28 18:18:52 +0000

    TODO: Ad React Router Dom * DataVisPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import Footer from './components/common/Footer'
 import Header from './components/common/Header'
 import LandingPage from './components/pages/LandingPage'
+// import DataVisPage from './components/pages/DataVisPage'
 
 function App() {
 
   return (
     <div className='w-[100vw] h-[100vh] m-0 flex flex-col align-centre text-center'>
       <Header />
+      {/* TODO: Add React Router Dom */}
       <LandingPage />
+      {/* <DataVisPage /> */}
       <Footer />
     </div>
   )
