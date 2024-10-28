ce11c40b1008594576eb4d270b32eba6f81917c7
habermoose <habermoose@gmail.com>
2024-10-28 16:43:35 -0400

    Systemic details 2

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -86,25 +86,24 @@ export default function LandingPage(props) {
           </div>
           <div className="flex flex-col gap-3">
             <div className="insights-details-header">
-              <h3>36%</h3>
+              <h3>5%</h3>
             </div>
             <div className="insights-details-header">
               <p>
-                By the end of the Trump administration, the average asylum
-                office grant rate had fallen 36% from an average of 44 percent
-                in fiscal year 2016 to 28 percent in fiscal year 20202.
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
               </p>
             </div>
           </div>
           <div className="flex flex-col gap-3">
             <div className="insights-details-header">
-              <h3>36%</h3>
+              <h3>6x Lower</h3>
             </div>
             <div className="insights-details-header">
               <p>
-                By the end of the Trump administration, the average asylum
-                office grant rate had fallen 36% from an average of 44 percent
-                in fiscal year 2016 to 28 percent in fiscal year 20202.
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was 6 times lower than the San Francisco
+                asylum office.
               </p>
             </div>
           </div>
