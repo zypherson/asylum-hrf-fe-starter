94a0b4fe9bef27dab51d14a74dfd5d33d18af424
habermoose <habermoose@gmail.com>
2024-11-02 12:03:15 -0400

    env vars

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import './index.css';
 import { App } from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+require('dotenv').config();
+
 createRoot(document.getElementById('root')).render(
-  <Auth0Provider
-    domain='dev-qxlgmxhzixc2w1pm.us.auth0.com'
-    clientId='X6brpU1FkHIgivRrcnxbM4elrx56Flaz'
-    authorizationParams={{ redirect_uri: window.location.origin }}
-  >
+  <Auth0Provider domain={process.env.AUTH0_DOMAIN} clientId={process.env.AUTH0_CLIENT_ID} authorizationParams={{ redirect_uri: window.location.origin }}>
     <App />
   </Auth0Provider>
 );
